dd5ef0478031dd7eed73b8ac96b1fb3acaab468f
lyin <ling95613@163.com>
2018-02-28 15:24:46 +0800

    登录校验完善

--- program/src/routes/Test/Test.js
+++ program/src/routes/Test/Test.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { connect } from 'dva';
 import { verifyLogin } from '../../utils/tools';
 
+@connect()
 class Test extends React.Component {
   componentDidMount() {
     console.log('测试页面加载好了!');
     verifyLogin();
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'user/fetch',
+    });
   }
 
   render() {
