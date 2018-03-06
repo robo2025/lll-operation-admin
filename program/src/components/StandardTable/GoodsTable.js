f2f9cdaa55a9f454994baca6c30f82bf954ba312
lyin <ling95613@163.com>
2018-03-06 17:27:08 +0800

    enerything is ok

--- program/src/components/StandardTable/GoodsTable.js
+++ program/src/components/StandardTable/GoodsTable.js
@@ -2,7 +2,7 @@
  * @Author: lll
  * @Date: 2018-01-26 14:08:45
  * @Last Modified by: lll
- * @Last Modified time: 2018-03-06 11:51:17
+ * @Last Modified time: 2018-03-06 17:23:38
  */
 import React, { PureComponent, Fragment } from 'react';
 import moment from 'moment';
@@ -181,7 +181,7 @@ class GoodsTable extends PureComponent {
             <Divider type="vertical" />
             <a
               onClick={() => onPublish(record.id, 0)}
-              disabled={(record.audit_status !== 1) || (record.is_published === 0)}
+              disabled={(record.audit_status !== 1) || (record.is_publish === 0)}
             >
               下架
             </a>
