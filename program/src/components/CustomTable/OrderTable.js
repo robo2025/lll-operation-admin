52827f6d8606abf0732157a03a70a0a57cef552a
lyin <ling95613@163.com>
2018-05-21 13:55:25 +0800

    调整订单列表UI

--- program/src/components/CustomTable/OrderTable.js
+++ program/src/components/CustomTable/OrderTable.js
@@ -49,88 +49,75 @@ export default class OrderTable extends React.Component {
     const { selectedRowKeys, totalCallNo, isShowModal } = this.state;
     const { data, defaultPage, loading, total } = this.props;
 
-    const columns = [
-      {
-        title: '序号',
-        dataIndex: 'id',
-        key: 'id',
-        width: 60,
-        fixed: 'left',
-      },
-      {
-        title: '客户订单编号',
-        dataIndex: 'son_order_sn',
-        key: 'son_order_sn',
-        width: 200,
-        fixed: 'left',
-      },
-      {
-        title: '母订单编号',
-        dataIndex: 'order_sn',
-        key: 'order_sn',
-        width: 200,
-        fixed: 'left',
-      },
-      {
-        title: '供应商公司名称',
-        dataIndex: 'supplier_name',
-        key: 'supplier_name',
-        width: 150,
-        fixed: 'left',
-      },
-      {
-        title: '客户公司名称',
-        dataIndex: 'guest_name',
-        align: 'guest_name',
-        width: 150,
-        fixed: 'left',
-        render: val => `${val}`,
-      },
-      {
-        title: '最大发货日期',
-        dataIndex: 'max_delivery_time',
-        key: 'max_delivery_time',
-        width: 150,
-        render: text => (<span>{text}天</span>),
-      },
-      {
-        title: '交易总金额(元)',
-        dataIndex: 'total_money',
-        width: 150,
-        key: 'total_money',
-      },
-      {
-        title: '支付状态',
-        dataIndex: 'pay_status',
-        key: 'pay_status',
-        width: 150,
-        render: text => (<span>{mapPayStatus[text]}</span>),
-      },
-      {
-        title: '订单状态',
-        dataIndex: 'order_status',
-        key: 'order_status',
-        width: 150,
-        render: text => (<span>{mapOrderStatus[text - 1]}</span>),
-      },
-      {
-        title: '佣金(元)',
-        dataIndex: 'commission',
-        width: 150,
-        key: 'commission',
-      },
-      {
-        title: '下单时间',
-        dataIndex: 'add_time',
-        key: 'add_time',
-        sorter: true,
-        render: val => <span>{moment(Math.floor(val * 1000)).format('YYYY-MM-DD HH:mm:ss')}</span>,
-      },
-      {
-        title: '操作',
-        render: (text, record) => (
-          <Fragment>
-            {/* <Dropdown 
+    const columns = [{
+      title: '序号',
+      dataIndex: 'id',
+      key: 'id',
+      width: 60,
+      fixed: 'left',
+      render: (text, record, idx) => (<span>{idx + 1}</span>),
+    }, {
+      title: '客户订单编号',
+      dataIndex: 'son_order_sn',
+      key: 'son_order_sn',
+      width: 200,
+      fixed: 'left',
+    }, {
+      title: '母订单编号',
+      dataIndex: 'order_sn',
+      key: 'order_sn',
+      width: 200,
+    }, {
+      title: '供应商公司名称',
+      dataIndex: 'supplier_name',
+      key: 'supplier_name',
+      width: 150,
+    }, {
+      title: '客户公司名称',
+      dataIndex: 'guest_name',
+      align: 'guest_name',
+      width: 150,
+      render: val => `${val}`,
+    }, {
+      title: '最大发货日期',
+      dataIndex: 'max_delivery_time',
+      key: 'max_delivery_time',
+      width: 150,
+      render: text => (<span>{text}天</span>),
+    }, {
+      title: '交易总金额(元)',
+      dataIndex: 'total_money',
+      width: 150,
+      key: 'total_money',
+    }, {
+      title: '支付状态',
+      dataIndex: 'pay_status',
+      key: 'pay_status',
+      width: 150,
+      render: text => (<span>{mapPayStatus[text]}</span>),
+    }, {
+      title: '佣金(元)',
+      dataIndex: 'commission',
+      width: 150,
+      key: 'commission',
+    }, {
+      title: '下单时间',
+      dataIndex: 'add_time',
+      key: 'add_time',
+      sorter: true,
+      render: val => <span>{moment(Math.floor(val * 1000)).format('YYYY-MM-DD HH:mm:ss')}</span>,
+    }, {
+      title: '订单状态',
+      dataIndex: 'order_status',
+      key: 'order_status',
+      width: 100,
+      fixed: 'right',
+      render: text => (<span>{mapOrderStatus[text - 1]}</span>),
+    }, {
+      title: '操作',
+      render: (text, record) => (
+        <Fragment>
+          {/* <Dropdown 
             overlay={(
               <Menu onClick={(key) => { this.handleOrderClick(key); }}>
                 <Menu.Item key={`1-${text.id}`}>催单</Menu.Item>
@@ -143,14 +130,14 @@ export default class OrderTable extends React.Component {
                 订单处理<Icon type="down" />
               </a>
             </Dropdown> */}
-            <a onClick={() => { this.handleOrderClick(`2-${text.id}`); }} disabled={![1, 3].includes(text.order_status)}>订单取消</a>
-            <Divider type="vertical" />
-            <a href={'#/orders/list/detail?orderId=' + record.id}>查看</a>
-          </Fragment>
-        ),
-        width: 150,
-        fixed: 'right',
-      },
+          <a onClick={() => { this.handleOrderClick(`2-${text.id}`); }} disabled={![1, 3].includes(text.order_status)}>订单取消</a>
+          <Divider type="vertical" />
+          <a href={'#/orders/list/detail?orderId=' + record.id}>查看</a>
+        </Fragment>
+      ),
+      width: 150,
+      fixed: 'right',
+    },
     ];
 
     const paginationProps = {
