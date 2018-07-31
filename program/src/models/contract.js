722b14d448513d2382c835905194faa4b7446397
西瓜梨子01 <1289072216@qq.com>
2018-07-31 17:26:16 +0800

    合同列表新增和编辑，合同编号不唯一时提示

--- program/src/models/contract.js
+++ program/src/models/contract.js
@@ -4,49 +4,50 @@ import {
   addContract,
   queryContractDetail,
   deleteContract,
-  editContract
-} from "../services/contract";
-import { SUCCESS_STATUS } from "../constant/config";
+  editContract,
+} from '../services/contract';
+import { SUCCESS_STATUS } from '../constant/config';
+
 export default {
-  namespace: "contract",
+  namespace: 'contract',
   state: {
     contractList: [],
     supplierList: [],
     contractTotal: 0,
     supplierTotal: 0,
-    contractDetail: {}
+    contractDetail: {},
   },
   effects: {
     *fetch({ params, offset, limit, success, error }, { call, put }) {
       const res = yield call(queryContractList, { params, offset, limit });
       if (res.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === "function") {
+        if (typeof success === 'function') {
           success(res);
         }
-      } else if (typeof error === "function") {
+      } else if (typeof error === 'function') {
         error(res);
         return;
       }
       const { headers } = res;
       yield put({
-        type: "save",
+        type: 'save',
         payload: res.data,
-        headers
+        headers,
       });
     },
     *fetchContractDetail({ id, success, error }, { call, put }) {
       const res = yield call(queryContractDetail, { id });
       if (res.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === "function") {
+        if (typeof success === 'function') {
           success(res);
         }
-      } else if (typeof error === "function") {
+      } else if (typeof error === 'function') {
         error(res);
         return;
       }
       yield put({
-        type: "saveDetail",
-        payload: res.data
+        type: 'saveDetail',
+        payload: res.data,
       });
     },
     *fetchSupplierList(
@@ -55,74 +56,71 @@ export default {
     ) {
       const res = yield call(querySupplierList, { params, offset, limit });
       if (res.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === "function") {
+        if (typeof success === 'function') {
           success(res);
-        } else if (typeof error === "function") {
-          error(res);
-          return;
-        }
+        } 
+      } else if (typeof error === 'function') {
+        error(res);
+        return;
       }
       const { headers } = res;
       yield put({
-        type: "saveSupplier",
+        type: 'saveSupplier',
         payload: res.data,
-        headers
+        headers,
       });
     },
     *fetchAddContract({ params, success, error }, { call }) {
       const res = yield call(addContract, { params });
       if (res.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === "function") {
-          success(res);
-        } else if (typeof error === "function") {
-          error(res);
-          return;
-        }
+            if (typeof success === 'function') {
+            success(res);
+            } 
+        } else if (typeof error === 'function') {
+            error(res);
       }
     },
     *fetchDeleteContract({ id, success, error }, { call }) {
       const res = yield call(deleteContract, { id });
       if (res.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === "function") {
+        if (typeof success === 'function') {
           success(res);
-        } else if (typeof error === "function") {
-          error(res);
-          return;
         }
+      } else if (typeof error === 'function') {
+        error(res);
       }
     },
-    *fetchEditContract({ id,params, success, error }, { call }) {
-      const res = yield call(editContract, { id,params });
+    *fetchEditContract({ id, params, success, error }, { call }) {
+      const res = yield call(editContract, { id, params });
       if (res.rescode >> 0 === SUCCESS_STATUS) {
-        if (typeof success === "function") {
+        if (typeof success === 'function') {
           success(res);
-        } else if (typeof error === "function") {
-          error(res);
-          return;
-        }
+        } 
+      } else if (typeof error === 'function') {
+        error(res);
       }
-    }
+    },
   },
   reducers: {
     save(state, action) {
       return {
         ...state,
         contractList: action.payload,
-        contractTotal: action.headers["x-content-total"] >> 0
+        contractTotal: action.headers['x-content-total'] >> 0,
       };
     },
     saveSupplier(state, action) {
       return {
         ...state,
         supplierList: action.payload,
-        supplierTotal: action.headers["x-content-total"] >> 0
+        supplierTotal: action.headers['x-content-total'] >> 0,
       };
     },
     saveDetail(state, action) {
       return {
         ...state,
-        contractDetail: action.payload
+        contractDetail: action.payload,
       };
-    }
-  }
+    },
+  },
 };
